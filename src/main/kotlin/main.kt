325116baa0ae205cf30dc701cffac0b04e30c7f2
Kukuruzka7 <yarozalina@gmail.com>
2021-09-27 23:02:02 +0300

    Structure and prepare inputData was written and tested.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,74 @@
+import java.io.File
+
+const val DATA_BASES_DIR = "/"
+//место, где мы храним папку с базами данных
+
+enum class Command {
+    ADD, DEL, FIND, PRINT
+}
+
+enum class Error {
+    BAD_COMMAND
+}
+
+data class KeyVal(val key: String, val value: Any) {
+}
+
+// Нужно добавить --- вернуть много сразу значений получить по клучам из файла
+// add <dataBaseName> <key> <val> --- добавить ключ - значение в базу данных dataBaseName
+// del <dataBaseName> <key> --- удалить значение по ключу в базе данных dataBaseName
+// find <dataBaseName> <key> --- найти значение по ключу в базе данных dataBaseName
+// print <dataBaseName> --- вывести базу данных dataBaseName
+
 fun main(args: Array<String>) {
+    val command = Command.valueOf(args[0].uppercase())
+    when (command) {
+        Command.ADD -> add(getInputDir(args), getInputKeyVal(args))
+        Command.DEL -> del(getInputDir(args), getInputKey(args))
+        Command.FIND -> find(getInputDir(args), getInputKey(args))
+        Command.PRINT -> print(getInputDir(args))
+    }
+}
+
+fun getInputDir(args: Array<String>): File? {
+    if (args.size < 2) {
+        return null
+    }
+    return File(DATA_BASES_DIR + args[1])
+}
+
+fun getInputKeyVal(args: Array<String>): KeyVal? {
+    if (args.size < 4) {
+        return null
+    }
+    return KeyVal(args[2], args[3])
+}
+
+fun getInputKey(args: Array<String>): String? {
+    if (args.size < 3) {
+        return null
+    }
+    return args[2]
+}
+
+fun add(dataBase: File?, kv: KeyVal?) {
     TODO()
+}
+
+fun del(dataBase: File?, key: String?) {
+    TODO()
+}
+
+fun find(dataBase: File?, key: String?) {
+    TODO()
+}
+
+fun print(dataBase: File?) {
+    TODO("Not yet implemented")
+}
+
+fun printErr(error: Error) {
+    when (error) {
+        Error.BAD_COMMAND -> println("Ваша команда некорректна, попробуйте еще раз.")
+    }
 }
--- src/test/kotlin/Test1.kt
+++ src/test/kotlin/Test1.kt
@@ -1,9 +1,52 @@
-import kotlin.test.*
+import org.junit.jupiter.api.Test
+import java.io.File
+import kotlin.test.assertEquals
 
 internal class Test1 {
 
     @Test
-    fun test1() {
-        assert(true)
+    fun testGetInputDir() {
+        val args1 = arrayOf("add", "src", "sdfkj", "kjdsfhgshj")
+        val args2 = arrayOf("del", "src/sklfir")
+        val args3 = arrayOf("add")
+        val args4 = arrayOf("del", "sdfkj")
+        val args5 = emptyArray<String>()
+        assertEquals(File(DATA_BASES_DIR + "src"), getInputDir(args1))
+        assertEquals(File(DATA_BASES_DIR + "src/sklfir"), getInputDir(args2))
+        assertEquals(null, getInputDir(args3))
+        assertEquals(File(DATA_BASES_DIR + "sdfkj"), getInputDir(args4))
+        assertEquals(null, getInputDir(args5))
+    }
+
+    @Test
+    fun testGetInputKeyVal() {
+        val args1 = arrayOf("add", "src", "sdfkj", "kjdsfhgshj")
+        val args2 = arrayOf("add", "", "sdfkjsf  ", "k  jdsfhgshj")
+        val args3 = arrayOf("del", "src/sklfir")
+        val args4 = arrayOf("add")
+        val args5 = arrayOf("del", "sdfkj")
+        val args6 = emptyArray<String>()
+        assertEquals(KeyVal("sdfkj", "kjdsfhgshj"), getInputKeyVal(args1))
+        assertEquals(KeyVal("sdfkjsf  ", "k  jdsfhgshj"), getInputKeyVal(args2))
+        assertEquals(null, getInputKeyVal(args3))
+        assertEquals(null, getInputKeyVal(args4))
+        assertEquals(null, getInputKeyVal(args5))
+        assertEquals(null, getInputKeyVal(args6))
+    }
+
+    @Test
+    fun testGetInputKey() {
+        val args1 = arrayOf("add", "src", "sdfkj", "kjdsfhgshj")
+        val args2 = arrayOf("add", "", "sdfkjsf  ", "k  jdsfhgshj")
+        val args3 = arrayOf("del", "src/sklfir")
+        val args4 = arrayOf("add")
+        val args5 = arrayOf("del", "sdfkj")
+        val args6 = emptyArray<String>()
+        assertEquals("sdfkj", getInputKey(args1))
+        assertEquals("sdfkjsf  ", getInputKey(args2))
+        assertEquals(null, getInputKey(args3))
+        assertEquals(null, getInputKey(args4))
+        assertEquals(null, getInputKey(args5))
+        assertEquals(null, getInputKey(args6))
     }
 }
